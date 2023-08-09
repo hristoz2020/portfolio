61ba2959d9043c3c4d6923feaddde7199d304831
hristoz2020 <62563444+hristoz2020@users.noreply.github.com>
2023-08-09 15:59:36 +0300

    First commit

---
+++ src/redux/store.tsx
@@ -0,0 +1,43 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+	persistStore,
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import testSlice from "./slices/testSlice";
+
+const persistConfig = {
+	key: "root",
+	storage,
+};
+
+const rootReducer = combineReducers({
+    test: testSlice
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+	reducer: persistedReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [
+					FLUSH,
+					REHYDRATE,
+					PAUSE,
+					PERSIST,
+					PURGE,
+					REGISTER,
+				],
+			},
+		}),
+});
+
+export const persistor = persistStore(store);
