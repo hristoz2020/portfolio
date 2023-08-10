f744d42a79f58645134dd3f8e44e88b0e47c54c9
hristoz2020 <62563444+hristoz2020@users.noreply.github.com>
2023-08-10 16:48:03 +0300

    Added text and image in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,65 @@
+import { Avatar, Box, Typography } from "@mui/material";
 import { FC } from "react";
+import WavingHandIcon from "@mui/icons-material/WavingHand";
+import img from "../assets/images/image.ico";
 
 const Home: FC = () => {
+	return (
+		<Box
+			sx={{
+				nt: 8,
+				nb: 12,
+				pt: 18,
+				pb: 18,
+				display: "flex",
+				justifyContent: "center",
+				flexDirection: "column",
+				alignItems: "center",
+				"@media (min-width:800px)": {
+					flexDirection: "row",
+				},
+			}}
+			id="home"
+		>
+			<Box sx={{ p: 10}}>
+				<Typography
+					sx={{
+						color: "inherit",
+						display: "flex",
+						justifyContent: "left",
+                        alignItems: "center"
+					}}
+					variant="h4"
+				>
+					<WavingHandIcon color="inherit" />
+					Hi, I'm Hristoz!
+				</Typography>
 
-    return(<div>
-        <h1>Home page!</h1>
-    </div>)
-}
+				<Typography
+					sx={{
+						color: "inherit",
+						display: "flex",
+						justifyContent: "left",
+						alignItems: "center",
+					}}
+					variant="h6"
+				>
+					I'm interested in Front-End Development.
+				</Typography>
+			</Box>
+
+			<Avatar
+				alt="Your Alt Text"
+				src={img}
+				sx={{
+					width: 200,
+					height: 200,
+					m: "",
+					borderRadius: "50%",
+				}}
+			/>
+		</Box>
+	);
+};
 
 export default Home;
