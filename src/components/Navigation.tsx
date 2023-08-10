800bbbfacfc9380557ffc3271393d164f5343781
hristoz2020 <62563444+hristoz2020@users.noreply.github.com>
2023-08-10 18:08:41 +0300

    Fixed navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { AppBar, Toolbar, Typography, IconButton, Button } from "@mui/material";
+import {
+	AppBar,
+	Toolbar,
+	Typography,
+	IconButton,
+	Button,
+	Drawer,
+	List,
+	ListItem,
+	ListItemText,
+	useMediaQuery,
+	useTheme,
+} from "@mui/material";
 import CodeOutlinedIcon from "@mui/icons-material/CodeOutlined";
+import MenuIcon from "@mui/icons-material/Menu";
 import MaterialUISwitch from "./MaterialUISwitch";
 
 interface NavigationProps {
@@ -16,32 +29,62 @@ const routes = [
 ];
 
 const Navigation: React.FC<NavigationProps> = ({ toggleDarkMode }) => {
+	const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+	const theme = useTheme();
+	const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+	const toggleDrawer = () => {
+		setIsDrawerOpen(!isDrawerOpen);
+	};
+
 	return (
 		<AppBar position="static" color="default">
 			<Toolbar>
-				<IconButton color="inherit" edge="start" aria-label="emoji">
-					<CodeOutlinedIcon />
-				</IconButton>
-
+				<CodeOutlinedIcon />
 				<Typography variant="h6" sx={{ flexGrow: 1 }}>
 					Portfolio
 				</Typography>
-				{routes.map((linkItem) => (
-					<Button
-						component={Link}
-						to={linkItem.path}
+				{isMobile ? (
+					<IconButton
 						color="inherit"
-						key={linkItem.id}
+						edge="start"
+						aria-label="menu"
+						onClick={toggleDrawer}
 					>
-						{linkItem.title}
-					</Button>
-				))}
-				<MaterialUISwitch
-					sx={{ m: 1 }}
-					defaultChecked
-					onClick={toggleDarkMode}
-				/>
+						<MenuIcon />
+					</IconButton>
+				) : (
+					<>
+						{routes.map((linkItem) => (
+							<Button
+								component={Link}
+								to={linkItem.path}
+								color="inherit"
+								key={linkItem.id}
+							>
+								{linkItem.title}
+							</Button>
+						))}
+					</>
+				)}
+				<MaterialUISwitch defaultChecked onClick={toggleDarkMode} />
 			</Toolbar>
+			<Drawer anchor="right" open={isDrawerOpen} onClose={toggleDrawer}>
+				<List>
+					{routes.map((linkItem) => (
+						<ListItem
+							button
+							component={Link}
+							to={linkItem.path}
+							onClick={toggleDrawer}
+							key={linkItem.id}
+						>
+							<ListItemText primary={linkItem.title} />
+						</ListItem>
+					))}
+				</List>
+			</Drawer>
 		</AppBar>
 	);
 };
