464154d019e0853ad7a7072babaacb65812d726f
hristoz2020 <62563444+hristoz2020@users.noreply.github.com>
2023-08-17 15:15:59 +0300

    Added animation on scroll in about page

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
 import { Box, CardMedia, Typography } from "@mui/material";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const SkillCard = ({ name, image }: { name: string; image: string }) => {
+	useEffect(() => {
+		Aos.init({ duration: 2000 });
+	});
+
 	return (
 		<Box
 			sx={{
@@ -13,6 +20,7 @@ const SkillCard = ({ name, image }: { name: string; image: string }) => {
 				maxWidth: 120,
 				m: 2,
 			}}
+			data-aos="zoom-in"
 		>
 			<CardMedia
 				sx={{ height: 100, width: 100, m: 2 }}
