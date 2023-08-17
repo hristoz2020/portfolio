b382976904c6029f41579c0668b5dba087dd7db4
hristoz2020 <62563444+hristoz2020@users.noreply.github.com>
2023-08-17 17:33:19 +0300

    Added skill to skillsList

--- src/constants/skillsList.tsx
+++ src/constants/skillsList.tsx
@@ -1,42 +1,47 @@
 export const skillsList = [
-    {
-        id: 0,
-        skill: "JavaScript",
-        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
-    },
-    {
-        id: 1,
-        skill: "TypeScript",
-        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg"
-    },
-    {
-        id: 2,
-        skill: "ReactJS",
-        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg"
-    },
-    {
-        id: 3,
-        skill: "Redux",
-        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg"
-    },
-    {
-        id: 4,
-        skill: "HTML",
-        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg"
-    },
-    {
-        id: 5,
-        skill: "CSS",
-        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg"
-    },
-    {
-        id: 6,
-        skill: "Bootstrap",
-        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg"
-    },
-    {
-        id: 7,
-        skill: "MaterialUI",
-        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/materialui/materialui-original.svg"
-    },
-]
+	{
+		id: 0,
+		skill: "JavaScript",
+		image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+	},
+	{
+		id: 1,
+		skill: "TypeScript",
+		image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+	},
+	{
+		id: 2,
+		skill: "ReactJS",
+		image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+	},
+	{
+		id: 3,
+		skill: "Redux",
+		image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg",
+	},
+	{
+		id: 4,
+		skill: "HTML",
+		image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
+	},
+	{
+		id: 5,
+		skill: "CSS",
+		image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
+	},
+	{
+		id: 6,
+		skill: "Bootstrap",
+		image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg",
+	},
+	{
+		id: 7,
+		skill: "MaterialUI",
+		image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/materialui/materialui-original.svg",
+	},
+	{
+		id: 8,
+		skill: "Git",
+		image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
+	},
+];
