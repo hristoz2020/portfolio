42ab71b6d1fabb71571800be8f0c54c3e253a2dd
hristoz2020 <62563444+hristoz2020@users.noreply.github.com>
2023-08-18 17:33:52 +0300

    Added functionality to display a message when submit the form

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -1,38 +1,72 @@
-import { useState } from "react";
-import { TextField, Button, Grid, Container, Typography } from "@mui/material";
-import { Email, Person, Send } from "@mui/icons-material";
+import { useEffect, useRef, useState } from "react";
+import {
+	TextField,
+	Button,
+	Grid,
+	Container,
+	Typography,
+	Stack,
+	Alert,
+	IconButton,
+} from "@mui/material";
+import { Email, Person, Send, Close } from "@mui/icons-material";
+import emailjs from "@emailjs/browser";
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+const initialState = {
+	name: "",
+	email: "",
+	message: "",
+};
 
 const Contacts = () => {
-	const [name, setName] = useState("");
-	const [email, setEmail] = useState("");
-	const [message, setMessage] = useState("");
-	const [errors, setErrors] = useState<any>({
-		name: "",
-		email: "",
-		message: "",
+	const [formData, setFormData] = useState(initialState);
+	const [errors, setErrors] = useState<any>(initialState);
+	const [isSuccessfullReceived, setIsSuccessfullReceived] =
+		useState<boolean>(false);
+	const [isFailedReceive, setIsFailedReceive] = useState<boolean>(false);
+	const form = useRef<any>();
+
+	useEffect(() => {
+		Aos.init({ duration: 2000 });
 	});
 
-	const handleSubmit = (event: React.FormEvent) => {
-		event.preventDefault();
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
 
 		const newErrors: { [key: string]: string } = {};
-		if (!name) {
+		if (!formData.name) {
 			newErrors.name = "Please enter a name";
 		}
-		if (!email) {
+		if (!formData.email) {
 			newErrors.email = "Please enter an email";
-		} else if (!/\S+@\S+\.\S+/.test(email)) {
+		} else if (!/\S+@\S+\.\S+/.test(formData.email)) {
 			newErrors.email = "Invalid email address";
 		}
-		if (!message) {
+		if (!formData.message) {
 			newErrors.message = "Please enter a message";
 		}
 
 		if (Object.keys(newErrors).length === 0) {
-			console.log("Send", { name, email, message });
-			setName("");
-			setEmail("");
-			setMessage("");
+			emailjs
+				.sendForm(
+					"service_qss3lqk",
+					"template_s8s0sok",
+					form.current,
+					"cynXBX5-y0dnGZx3F"
+				)
+				.then(
+					(result) => {
+						console.log(result.text);
+						setIsSuccessfullReceived(true);
+					},
+					(error) => {
+						console.log(error.text);
+						setIsFailedReceive(true);
+					}
+				);
+			setFormData(initialState);
 			setErrors({});
 		} else {
 			setErrors(newErrors);
@@ -40,81 +74,163 @@ const Contacts = () => {
 	};
 
 	return (
-		<Container
-			sx={{
-				p: 10,
-			}}
-			maxWidth="sm"
-			id="/contacts"
-		>
-			<Typography
-				variant="h5"
-				align="center"
-				fontFamily={"monospace"}
-				gutterBottom
-			>
-				Contacts:
-			</Typography>
-			<form onSubmit={handleSubmit}>
-				<Grid container spacing={2}>
-					<Grid item xs={12}>
-						<TextField
-							label="Name"
-							variant="outlined"
-							fullWidth
-							value={name}
-							onChange={(e) => setName(e.target.value)}
-							error={Boolean(errors.name)}
-							helperText={errors.name}
-							InputProps={{
-								startAdornment: (
-									<Person sx={{ width: "30px", pr: 1 }} />
-								),
-							}}
-						/>
-					</Grid>
-					<Grid item xs={12}>
-						<TextField
-							label="Email"
-							variant="outlined"
-							fullWidth
-							value={email}
-							onChange={(e) => setEmail(e.target.value)}
-							error={Boolean(errors.email)}
-							helperText={errors.email}
-							InputProps={{
-								startAdornment: (
-									<Email sx={{ width: "30px", pr: 1 }} />
-								),
-							}}
-						/>
-					</Grid>
-					<Grid item xs={12}>
-						<TextField
-							label="Message"
-							variant="outlined"
-							multiline
-							rows={4}
-							fullWidth
-							value={message}
-							onChange={(e) => setMessage(e.target.value)}
-							error={Boolean(errors.message)}
-							helperText={errors.message}
-						/>
-					</Grid>
-				</Grid>
-				<Button
-					type="submit"
-					variant="contained"
-					color="primary"
-					endIcon={<Send />}
-					sx={{ fontFamily: "monospace", fontSize: "14px", mt: 2 }}
-					fullWidth
+		<>
+			{isSuccessfullReceived && (
+				<Stack
+					sx={{ width: "100%", position: "fixed", top: 0, left: 0 }}
+					spacing={2}
+					data-aos="flip-up"
 				>
-					Send
-				</Button>
-			</form>
-		</Container>
+					<Alert
+						severity="success"
+						sx={{ backgroundColor: "darkgray", boxShadow: 10 }}
+						action={
+							<IconButton
+								aria-label="close"
+								size="small"
+								onClick={() => {
+									setIsSuccessfullReceived(false);
+								}}
+							>
+								<Close fontSize="medium" color="inherit" />
+							</IconButton>
+						}
+					>
+						Your message has been successfully received!
+					</Alert>
+				</Stack>
+			)}
+			{isFailedReceive && (
+				<Stack
+					sx={{
+						width: "100%",
+						position: "fixed",
+						top: 0,
+						left: 0,
+					}}
+					spacing={2}
+					data-aos="flip-up"
+				>
+					<Alert
+						severity="error"
+						sx={{ backgroundColor: "darkgray", boxShadow: 10 }}
+						action={
+							<IconButton
+								aria-label="close"
+								size="small"
+								onClick={() => {
+									setIsFailedReceive(false);
+								}}
+							>
+								<Close fontSize="medium" color="inherit" />
+							</IconButton>
+						}
+					>
+						Oops something went wrong. Try again later!
+					</Alert>
+				</Stack>
+			)}
+			<Container
+				sx={{
+					py: 10,
+				}}
+				maxWidth="sm"
+				data-aos="flip-left"
+				data-aos-easing="ease-out-cubic"
+				data-aos-duration="2000"
+				id="/contacts"
+			>
+				<Typography
+					variant="h5"
+					align="center"
+					fontFamily={"monospace"}
+					gutterBottom
+				>
+					Contacts:
+				</Typography>
+				<form ref={form} onSubmit={handleSubmit}>
+					<Grid container spacing={2}>
+						<Grid item xs={12}>
+							<TextField
+								label="Name"
+								variant="outlined"
+								fullWidth
+								value={formData.name}
+								name="name"
+								onChange={(e) =>
+									setFormData({
+										...formData,
+										name: e.target.value,
+									})
+								}
+								error={Boolean(errors.name)}
+								helperText={errors.name}
+								InputProps={{
+									startAdornment: (
+										<Person sx={{ width: "30px", pr: 1 }} />
+									),
+								}}
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								label="Email"
+								variant="outlined"
+								fullWidth
+								value={formData.email}
+								name="email"
+								onChange={(e) =>
+									setFormData({
+										...formData,
+										email: e.target.value,
+									})
+								}
+								error={Boolean(errors.email)}
+								helperText={errors.email}
+								InputProps={{
+									startAdornment: (
+										<Email sx={{ width: "30px", pr: 1 }} />
+									),
+								}}
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								label="Message"
+								variant="outlined"
+								multiline
+								rows={4}
+								fullWidth
+								value={formData.message}
+								name="message"
+								onChange={(e) =>
+									setFormData({
+										...formData,
+										message: e.target.value,
+									})
+								}
+								error={Boolean(errors.message)}
+								helperText={errors.message}
+							/>
+						</Grid>
+					</Grid>
+					<Button
+						type="submit"
+						variant="contained"
+						color="primary"
+						endIcon={<Send />}
+						sx={{
+							fontFamily: "monospace",
+							fontSize: "14px",
+							mt: 2,
+						}}
+						fullWidth
+					>
+						Send
+					</Button>
+				</form>
+			</Container>
+		</>
 	);
 };
 
