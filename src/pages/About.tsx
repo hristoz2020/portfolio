e8b1552e223d11eda5e5a8ebbfce0466a4f78ae5
hristoz2020 <62563444+hristoz2020@users.noreply.github.com>
2023-08-22 11:00:37 +0300

    Added education to skills

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,15 @@
-import { Box, Typography } from "@mui/material";
-import { FC } from "react";
+import { FC, useEffect } from "react";
+import { Box, Typography, Link } from "@mui/material";
 import SkillCard from "../components/SkillCard";
 import { skillsList } from "../constants/skillsList";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const About: FC = () => {
+	useEffect(() => {
+		Aos.init({ duration: 2500 });
+	});
+
 	return (
 		<Box sx={{ borderTop: 1, borderBottom: 1, p: 12 }} id="/about">
 			<Typography
@@ -31,6 +37,106 @@ const About: FC = () => {
 						key={skill.id}
 					/>
 				))}
+			</Box>
+
+			<Typography
+				align="center"
+				variant="h5"
+				fontFamily={"monospace"}
+				sx={{ py: 4 }}
+			>
+				Education:
+			</Typography>
+			<Box
+				sx={{
+					display: "flex",
+					flexDirection: "row",
+					flexWrap: "wrap",
+					justifyContent: "center",
+					margin: "0 auto",
+					maxWidth: "580px",
+				}}
+			>
+				<Box
+					sx={{
+						display: "flex",
+						alignItems: "center",
+						border: 1,
+						borderRadius: 2,
+						boxShadow: 8,
+						my: 1,
+						p: 2,
+					}}
+					data-aos="zoom-in"
+				>
+					<Box>
+						<Typography variant="h6" fontFamily={"monospace"}>
+							Programming Education with JavaScript
+						</Typography>
+						<Typography variant="body2" fontFamily={"monospace"}>
+							<Link
+								href="https://www.tugab.bg/"
+								target="_blank"
+								rel="noopener noreferrer"
+								variant="body2"
+								fontFamily="monospace"
+								sx={{
+									textDecoration: "underline",
+									display: "block",
+									mt: 2,
+								}}
+							>
+								Software University SoftUni
+							</Link>
+						</Typography>
+					</Box>
+					<img
+						src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Logo_Software_University_%28SoftUni%29_-_blue.png/535px-Logo_Software_University_%28SoftUni%29_-_blue.png"
+						width="100px"
+					/>
+				</Box>
+				<Box
+					sx={{
+						display: "flex",
+						alignItems: "center",
+						border: 1,
+						borderRadius: 2,
+						boxShadow: 8,
+						my: 1,
+						p: 2,
+					}}
+					data-aos="zoom-in"
+				>
+					<Box>
+						<Typography variant="h6" fontFamily={"monospace"}>
+							Master's degree of Computer Systems and Technologies
+						</Typography>
+						<Typography
+							variant="body2"
+							fontFamily={"monospace"}
+							sx={{ textDecoration: "underline" }}
+						>
+							<Link
+								href="https://www.tugab.bg/"
+								target="_blank"
+								rel="noopener noreferrer"
+								variant="body2"
+								fontFamily="monospace"
+								sx={{
+									textDecoration: "underline",
+									display: "block",
+									mt: 2,
+								}}
+							>
+								Technical University of Gabrovo
+							</Link>
+						</Typography>
+					</Box>
+					<img
+						src="https://rectors.bg/images/members/TU-GABROVO.png"
+						width="110px"
+					/>
+				</Box>
 			</Box>
 		</Box>
 	);
